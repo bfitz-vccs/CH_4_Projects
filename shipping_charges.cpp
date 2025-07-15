c9756b136b4415f479a26bf6e4a863676a7c3f1a
bfitz-vccs <bf2877@email.vccs.edu>
2025-07-14 22:15:50 -0400

    Changed the shipping charge formula to measure 500 mile increments and formatted output to two decimal places.

--- shipping_charges.cpp
+++ shipping_charges.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <iomanip>
 
 int main(){
 
     float weight;
-    float distance;
+    int distance;
+    std::cout << std::setprecision(2) << std::fixed;
     std::cout << "Please enter in the package weight (kg): ";
     std::cin >> weight;
     std::cout << "Please enter in the shipping distance in miles: ";
@@ -12,24 +14,53 @@ int main(){
     if (weight < 0 || weight > 20){
         std::cout << "Invalid weight option." << std::endl;
     }
+
     else if (distance < 10 || distance > 3000){
         std::cout << "Invalid distance option." << std::endl;
     }
+
     else if (weight <= 2){
-        float charge = 1.10 * distance;
-        std::cout << "Shipping charge: $" << charge << std::endl;
+        if (distance % 500 == 0){
+            float charge = 1.10 * (distance / 500);
+            std::cout << "Shipping charge: $" << charge << std::endl;
+        }
+        else{
+            float charge = 1.10 * ((distance / 500) + 1);
+            std::cout << "Shipping charge: $" << charge << std::endl;
+        }
     }
+
     else if (weight > 2 && weight <= 6){
-        float charge = 2.20 * distance;
-        std::cout << "Shipping charge: $" << charge << std::endl;
+        if (distance % 500 == 0){
+            float charge = 2.20 * (distance / 500);
+            std::cout << "Shipping charge: $" << charge << std::endl;
+        }
+        else{
+            float charge = 2.20 * ((distance / 500) + 1);
+            std::cout << "Shipping charge: $" << charge << std::endl;
+        }
     }
+
     else if (weight > 6 && weight <= 10){
-        float charge = 3.70 * distance;
-        std::cout << "Shipping charge: $" << charge << std::endl;
+        if (distance % 500 == 0){
+            float charge = 3.70 * (distance / 500);
+            std::cout << "Shipping charge: $" << charge << std::endl;
+        }
+        else{
+            float charge = 3.70 * ((distance / 500) + 1);
+            std::cout << "Shipping charge: $" << charge << std::endl;
+        }
     }
+
     else if (weight > 10 && weight <= 20){
-        float charge = 4.80 * distance;
-        std::cout << "Shipping charge: $" << charge << std::endl;
+        if (distance % 500 == 0){
+            float charge = 4.80 * (distance / 500);
+            std::cout << "Shipping charge: $" << charge << std::endl;
+        }
+        else{
+            float charge = 4.80 * ((distance / 500) + 1);
+            std::cout << "Shipping charge: $" << charge << std::endl;
+        }
     }
 
     return 0;
